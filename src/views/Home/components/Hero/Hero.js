e05e7732c1f9f30ad09af698bcbc5003436c006b
Dev Dork <robertcjohnson1984@gmail.com>
2023-10-18 16:41:25 -0600

    update hero

--- src/views/Home/components/Hero/Hero.js
+++ src/views/Home/components/Hero/Hero.js
@@ -1,81 +1,13 @@
 import React from 'react';
+import Slider from 'react-slick';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { alpha, useTheme } from '@mui/material/styles';
-// import Slider from "react-slick";
 
 import Container from 'components/Container';
-
-const images = [
-  {
-    group: [
-      {
-        cover:
-          'https://assets.maccarianagency.com/screenshots/the-front/img1.png',
-        coverDark:
-          'https://assets.maccarianagency.com/screenshots/the-front/img1--dark.png',
-      },
-      {
-        cover:
-          'https://assets.maccarianagency.com/screenshots/the-front/img4.png',
-        coverDark:
-          'https://assets.maccarianagency.com/screenshots/the-front/img4--dark.png',
-      },
-    ],
-  },
-  {
-    group: [
-      {
-        cover:
-          'https://assets.maccarianagency.com/screenshots/the-front/img13.png',
-        coverDark:
-          'https://assets.maccarianagency.com/screenshots/the-front/img13--dark.png',
-      },
-      {
-        cover:
-          'https://assets.maccarianagency.com/screenshots/the-front/img10.png',
-        coverDark:
-          'https://assets.maccarianagency.com/screenshots/the-front/img10--dark.png',
-      },
-      {
-        cover:
-          'https://assets.maccarianagency.com/screenshots/the-front/img7.png',
-        coverDark:
-          'https://assets.maccarianagency.com/screenshots/the-front/img7--dark.png',
-      },
-    ],
-  },
-  {
-    group: [
-      {
-        cover:
-          'https://assets.maccarianagency.com/screenshots/the-front/img6.png',
-        coverDark:
-          'https://assets.maccarianagency.com/screenshots/the-front/img6--dark.png',
-      },
-      {
-        cover:
-          'https://assets.maccarianagency.com/screenshots/the-front/img24.png',
-        coverDark:
-          'https://assets.maccarianagency.com/screenshots/the-front/img24--dark.png',
-      },
-      {
-        cover:
-          'https://assets.maccarianagency.com/screenshots/the-front/img17.png',
-        coverDark:
-          'https://assets.maccarianagency.com/screenshots/the-front/img17--dark.png',
-      },
-      {
-        cover:
-          'https://assets.maccarianagency.com/screenshots/the-front/img12.png',
-        coverDark:
-          'https://assets.maccarianagency.com/screenshots/the-front/img12--dark.png',
-      },
-    ],
-  },
-];
 
 const Hero = () => {
   const theme = useTheme();
@@ -83,149 +15,211 @@ const Hero = () => {
     defaultMatches: true,
   });
 
+  const LeftSide = () => (
+    <Box data-aos={isMd ? 'fade-right' : 'fade-up'}>
+      <Typography
+        sx={{
+          textTransform: 'uppercase',
+          fontWeight: 'medium',
+        }}
+        gutterBottom
+        color={'text.secondary'}
+      >
+        Coworking spaces
+      </Typography>
+      <Box marginBottom={2}>
+        <Typography
+          variant="h2"
+          color="text.primary"
+          sx={{
+            fontWeight: 700,
+          }}
+        >
+          Coworking{' '}
+          <Typography
+            color={'primary'}
+            component={'span'}
+            variant={'inherit'}
+            sx={{
+              background: `linear-gradient(180deg, transparent 82%, ${alpha(
+                theme.palette.secondary.main,
+                0.3,
+              )} 0%)`,
+            }}
+          >
+            made simple
+          </Typography>
+        </Typography>
+      </Box>
+      <Box marginBottom={3}>
+        <Typography variant="h6" component="p" color="text.secondary">
+          For entrepreneurs, startups and freelancers. Discover coworking spaces
+          designed to inspire and to connect you to a community of motivated
+          people.
+        </Typography>
+      </Box>
+      <Box
+        display="flex"
+        flexDirection={{ xs: 'column', sm: 'row' }}
+        alignItems={{ xs: 'stretched', sm: 'flex-start' }}
+      >
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          fullWidth={isMd ? false : true}
+        >
+          Book a space
+        </Button>
+        <Box
+          component={Button}
+          variant="outlined"
+          color="primary"
+          size="large"
+          marginTop={{ xs: 2, sm: 0 }}
+          marginLeft={{ sm: 2 }}
+          fullWidth={isMd ? false : true}
+        >
+          Browse spaces
+        </Box>
+      </Box>
+    </Box>
+  );
+
+  const RightSide = () => {
+    const sliderOpts = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+    };
+
+    return (
+      <Box
+        sx={{
+          height: { xs: 'auto', md: 1 },
+          '& .slick-slide img': {
+            objectFit: 'cover',
+          },
+          '& .slick-list, & .slick-slider, & .slick-track, & .slick-slide > div': {
+            height: { xs: 'auto', md: 1 },
+          },
+          '& .slick-prev, & .slick-next': {
+            zIndex: 2,
+            bottom: 0,
+            top: '100%',
+            left: '100%',
+            right: 0,
+            transform: `translate(-100%, calc(-100% - ${theme.spacing(2)}))`,
+            marginLeft: theme.spacing(-2),
+            width: 32,
+            height: 32,
+            '&:before': {
+              fontSize: 32,
+            },
+          },
+          '& .slick-prev': {
+            marginLeft: theme.spacing(-7),
+          },
+        }}
+      >
+        <Slider {...sliderOpts}>
+          {[
+            'https://assets.maccarianagency.com/backgrounds/img1.jpg',
+            'https://assets.maccarianagency.com/backgrounds/img3.jpg',
+            'https://assets.maccarianagency.com/backgrounds/img24.jpg',
+            'https://assets.maccarianagency.com/backgrounds/img25.jpg',
+          ].map((item) => (
+            <Box
+              key={item}
+              component={'img'}
+              loading="lazy"
+              src={item}
+              height={{ xs: 'auto', md: 1 }}
+              maxHeight={{ xs: 300, md: 1 }}
+              width={1}
+              maxWidth={1}
+            />
+          ))}
+        </Slider>
+      </Box>
+    );
+  };
+
   return (
     <Box
       sx={{
-        backgroundImage: `linear-gradient(to bottom, ${alpha(
-          theme.palette.background.paper,
-          0,
-        )}, ${alpha(theme.palette.alternate.main, 1)} 100%)`,
-        backgroundRepeat: 'repeat-x',
-        position: 'relative',
+        width: 1,
+        height: 1,
+        overflow: 'hidden',
       }}
     >
-      <Box paddingY={{ xs: 0, sm: '4rem', md: '8rem' }}>
-        <Container>
-          <Box maxWidth={{ xs: 1, sm: '50%' }}>
-            <Typography
-              variant="h2"
-              color="text.primary"
-              gutterBottom
+      <Container paddingX={0} paddingY={0} maxWidth={{ sm: 1, md: 1236 }}>
+        <Box
+          display={'flex'}
+          flexDirection={{ xs: 'column', md: 'row' }}
+          position={'relative'}
+          minHeight={{ xs: 'auto', md: 600 }}
+        >
+          <Box
+            width={1}
+            order={{ xs: 2, md: 1 }}
+            display={'flex'}
+            alignItems={'center'}
+          >
+            <Container>
+              <LeftSide />
+            </Container>
+          </Box>
+          <Box
+            sx={{
+              flex: { xs: '0 0 100%', md: '0 0 50%' },
+              position: 'relative',
+              maxWidth: { xs: '100%', md: '50%' },
+              order: { xs: 1, md: 2 },
+            }}
+          >
+            <Box
               sx={{
-                fontWeight: 700,
+                width: { xs: 1, md: '50vw' },
+                height: '100%',
+                position: 'relative',
               }}
             >
-              Turn your ideas
-              <br />
-              into{' '}
-              <Typography
-                color={'primary'}
-                component={'span'}
-                variant={'inherit'}
+              <Box
                 sx={{
-                  background: `linear-gradient(180deg, transparent 82%, ${alpha(
-                    theme.palette.secondary.main,
-                    0.3,
-                  )} 0%)`,
+                  width: '100%',
+                  height: '100%',
+                  overflow: 'hidden',
                 }}
               >
-                success.
-              </Typography>
-            </Typography>
-            <Typography
-              variant="h6"
-              component="p"
-              color="text.secondary"
-              sx={{ fontWeight: 400 }}
-            >
-              theFront will make your product look modern and professional while
-              saving you precious time.
-            </Typography>
-            <Box
-              display="flex"
-              flexDirection={{ xs: 'column', sm: 'row' }}
-              alignItems={{ xs: 'stretched', sm: 'flex-start' }}
-              marginTop={4}
-            >
-              <Button
-                component={'a'}
-                variant="contained"
-                color="primary"
-                size="large"
-                fullWidth={isMd ? false : true}
-                href={'/home'}
-              >
-                View pages
-              </Button>
-              <Box
-                marginTop={{ xs: 2, sm: 0 }}
-                marginLeft={{ sm: 2 }}
-                width={{ xs: '100%', md: 'auto' }}
-              >
-                <Button
-                  component={'a'}
-                  href={'https://thefront.maccarianagency.com/docs/introduction'}
-                  target={'blank'}
-                  variant="outlined"
-                  color="primary"
-                  size="large"
-                  fullWidth={isMd ? false : true}
+                <Box
+                  sx={{
+                    overflow: 'hidden',
+                    left: '0%',
+                    width: 1,
+                    height: 1,
+                    position: { xs: 'relative', md: 'absolute' },
+                    clipPath: {
+                      xs: 'none',
+                      md: 'polygon(10% 0%, 100% 0, 100% 100%, 0% 100%)',
+                    },
+                    shapeOutside: {
+                      xs: 'none',
+                      md: 'polygon(10% 0%, 100% 0, 100% 100%, 0% 100%)',
+                    },
+                  }}
                 >
-                  Documentation
-                </Button>
+                  <RightSide />
+                </Box>
               </Box>
             </Box>
           </Box>
-        </Container>
-        <Box
-          sx={{
-            transform: 'rotate(-20deg)',
-            display: { xs: 'none', sm: 'block' },
-          }}
-        >
-          <Box
-            display={'flex'}
-            width={'50rem'}
-            left={'50%'}
-            top={0}
-            position={'absolute'}
-            sx={{ transform: 'translate3d(20%, -50%, 0)' }}
-          >
-            {images.map((item, i) => (
-              <Box key={i} marginTop={{ sm: -(i * 16) }} marginX={1}>
-                {item.group.map((g, j) => (
-                  <Box
-                    key={j}
-                    padding={1}
-                    bgcolor={'background.paper'}
-                    borderRadius={2}
-                    boxShadow={3}
-                    marginTop={2}
-                  >
-                    <Box
-                      component={'img'}
-                      loading="lazy"
-                      src={
-                        theme.palette.mode === 'dark' ? g.coverDark : g.cover
-                      }
-                      height={1}
-                      width={1}
-                      maxWidth={320}
-                    />
-                  </Box>
-                ))}
-              </Box>
-            ))}
-          </Box>
         </Box>
-      </Box>
-      <Box
-        component={'svg'}
-        preserveAspectRatio="none"
-        xmlns="http://www.w3.org/2000/svg"
-        x="0px"
-        y="0px"
-        viewBox="0 0 1920 100.1"
-        sx={{
-          width: '100%',
-          marginBottom: theme.spacing(-1),
-        }}
-      >
-        <path
-          fill={theme.palette.background.paper}
-          d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
-        ></path>
-      </Box>
+      </Container>
+      <Divider />
     </Box>
   );
 };
